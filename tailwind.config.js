24bc76404777e17f500ef3f10a59b0829382a840
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-03-21 00:16:40 +0800

    fix tailwindcss rwd

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,11 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'sm': '480px',
-      'md': '547px',
-      'lg': '768px',
-      'xl': '1024px',
-      '2xl': '1680px',
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      '2xl': '1400px',
     },
     extend: {
       colors: {
